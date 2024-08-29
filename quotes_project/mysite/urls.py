02d036ed5b1f3f0da539f1a962012ae72f62fc1a
mzLeVit (Vitaliy Lemanskyi) <160911095+mzLeVit@users.noreply.github.com>
2024-08-29 13:42:27 +0300

    Update urls.py

--- quotes_project/mysite/urls.py
+++ quotes_project/mysite/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from quotes_project.mysite.mysite import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
@@ -9,4 +10,8 @@ urlpatterns = [
     path('add-quote/', views.add_quote, name='add_quote'),
     path('tags/<str:tag>/', views.quotes_by_tag, name='quotes_by_tag'),
     path('top-tags/', views.top_tags, name='top_tags'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
